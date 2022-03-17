cf24d5d80d6ce1359cdbb543356593beb619beaa
songtaekwan <101688746+songtaekwan@users.noreply.github.com>
2022-03-17 11:10:20 +0900

    Update and rename 123 to python_project1.py

---
+++ 20220317/python_project1.py
@@ -0,0 +1,41 @@
+#001
+print("Hello World")
+
+#002
+print("Mary\'s cosmeics")
+
+#003
+print("신씨가 소리질렀다. \"도둑이야\".")
+
+#004
+print("c:\windows")
+
+#005
+print("안녕하세요.\n만나서\t\t반갑습니다.")
+#\n은 줄바꿈 \t는 일정공간을 띄어준다.
+
+#051
+movie_rank=['닥터스트레인지','스플릿','럭키']
+print(movie_rank)
+
+#052
+new_movie=['배트맨']
+print(movie_rank+new_movie)
+
+#059
+cook=["피자","김밥","만두","양념치킨","족발","피자","김치만두","쫄면","소시지","라면","팥빙수","김치전"]
+print(len(cook))
+
+#060
+nums=[1,2,3,4,5]
+result=sum(nums)
+print("실행 예")
+print(result/len(nums))
+
+#102    결과 값은 false가 나온다
+
+#103    결과 값은 true가 나온다
+
+#104    결과 값은 true가 나온다
+
+#105    결과 값은 true가 나온다
